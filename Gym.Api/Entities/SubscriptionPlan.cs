53d3429ac5c5ee498f45cc191bbc178a4729c0cf
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 01:33:22 +0200

    feat(subscriptionplan): add SubscriptionPlan Entity admin add SubscriptionPlan contain the type of membership

---
+++ Gym.Api/Entities/SubscriptionPlan.cs
@@ -0,0 +1,17 @@
+﻿namespace Gym.Api.Entities;
+
+public class SubscriptionPlan
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public bool UnlimitedDailyEntries { get; set; }
+
+    //  class‑booking limits
+    public int MaxClassBookingsPerDay { get; set; } = 1;
+    public int MaxClassBookingsInFuture { get; set; } = 3;
+
+    // Freeze rules
+    public int MaxFreezesPerYear { get; set; } = 2;
+    public int MaxFreezeDays { get; set; } = 30;
+
+}
