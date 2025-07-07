dd21ae5ff25dfb1602e99a2e34aabc39e76dbdf8
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 22:46:18 +0200

    feat(startup): configure services and database connection

---
+++ Gym.Api/ConfigurationService.cs
@@ -0,0 +1,25 @@
+﻿using Gym.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Gym.Api;
+
+public static class ConfigurationService
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextConfig(configuration);
+        return services;
+    }
+    public static IServiceCollection AddDbContextConfig(this IServiceCollection services,IConfiguration configuration)
+    {
+        var connectionString=configuration.GetConnectionString("DefaultConnection")??
+
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+
+        options.UseSqlServer(connectionString));
+        return services;
+    }
+}
