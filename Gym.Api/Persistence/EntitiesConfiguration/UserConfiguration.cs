3985d4822102d1dfc652f8d234a08be7970e6485
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 22:41:12 +0200

    feat(user): add configuration for ApplicationUser entity
    Set max length for FirstName and LastName using EF Core configuration.

---
+++ Gym.Api/Persistence/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Gym.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gym.Api.Persistence.EntitiesConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x=>x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
+    }
+}
