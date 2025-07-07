fdbb6ed29f3c48c502c6d1db0d6e91c50e33ef4d
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 23:16:52 +0200

    feat(role): add  roles to in database

---
+++ Gym.Api/Seeds/DefaultRoles.cs
@@ -0,0 +1,18 @@
+﻿using Gym.Api.Consts;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gym.Api.Seeds;
+
+public static class DefaultRoles
+{
+    public static async Task SeedAsync(RoleManager<IdentityRole> roleManager)
+    {
+        if(!roleManager.Roles.Any())
+        {
+            await roleManager.CreateAsync(new IdentityRole { Name = AppRoles.Admin });
+            await roleManager.CreateAsync(new IdentityRole { Name = AppRoles.Staff }); 
+            await roleManager.CreateAsync(new IdentityRole { Name = AppRoles.Trainer });
+            await roleManager.CreateAsync(new IdentityRole { Name = AppRoles.Member});
+        }
+    }
+}
