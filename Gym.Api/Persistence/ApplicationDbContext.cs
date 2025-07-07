6809e814ba90b5fc4111616426c55adb40adfd7a
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 22:00:09 +0200

    Add project files.

---
+++ Gym.Api/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Gym.Api.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+namespace Gym.Api.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser> (options)
+{
+
+}
