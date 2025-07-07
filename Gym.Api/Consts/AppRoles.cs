edd7e900ece6eeb3f8e08419e2ffbebeec2bc0a6
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 23:16:15 +0200

    feat(role): add roles name

---
+++ Gym.Api/Consts/AppRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Gym.Api.Consts;
+
+public static class AppRoles
+{
+    public const string Admin = "Admin";
+    public const string Staff = "Staff";
+    public const string Member = "Member";
+    public const string Trainer = "Trainer";
+}
