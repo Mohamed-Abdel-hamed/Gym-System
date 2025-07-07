afbeeaccec8b63bd1d18e9b4da2db495de36e60a
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 22:44:16 +0200

    feat(startup): configure services and database connection

--- Gym.Api/Program.cs
+++ Gym.Api/Program.cs
@@ -1,9 +1,13 @@
+using Gym.Api;
+using Gym.Api.Entities;
+using Gym.Api.Persistence;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddServices(builder.Configuration);
 builder.Services.AddControllers();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
