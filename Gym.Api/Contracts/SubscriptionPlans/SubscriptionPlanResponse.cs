920a4c256aa7118b2d7678f4c86b0dc25e8f2373
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 01:43:03 +0200

    feat(subscriptionplan): add response that will return to end user

---
+++ Gym.Api/Contracts/SubscriptionPlans/SubscriptionPlanResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Gym.Api.Contracts.SubscriptionPlans;
+
+public record SubscriptionPlanResponse
+    (
+     string Name,
+     bool UnlimitedDailyEntries,
+     int MaxClassBookingsPerDay ,
+     int MaxClassBookingsInFuture,
+     int MaxFreezesPerYear,
+     int MaxFreezeDays
+    );
