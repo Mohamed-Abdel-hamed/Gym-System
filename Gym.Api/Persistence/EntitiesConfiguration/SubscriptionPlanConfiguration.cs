bfded9a8ebb655dd8bf4a73e0fa86cb99ec98144
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 01:37:28 +0200

    feat(subscriptionplan): add configuration to SubscriptionPlan make length of name is 100 chars.

---
+++ Gym.Api/Persistence/EntitiesConfiguration/SubscriptionPlanConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Gym.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gym.Api.Persistence.EntitiesConfiguration;
+
+public class SubscriptionPlanConfiguration : IEntityTypeConfiguration<SubscriptionPlan>
+{
+    public void Configure(EntityTypeBuilder<SubscriptionPlan> builder)
+    {
+        builder.Property(x => x.Name).HasMaxLength(100);
+    }
+}
