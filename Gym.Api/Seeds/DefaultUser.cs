2bda8884451fc795c7011e9d712b2988e4c4a976
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 00:36:16 +0200

    feat(auth): seed new user as admin

---
+++ Gym.Api/Seeds/DefaultUser.cs
@@ -0,0 +1,27 @@
+﻿using Gym.Api.Consts;
+using Gym.Api.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gym.Api.Seeds;
+
+public static class DefaultUser
+{
+    public static async Task SeedAdminUserAsync(UserManager<ApplicationUser> userManager)
+    {
+            ApplicationUser admin = new()
+            {
+                UserName = "admin",
+                Email = "admin@bookify.com",
+                FirstName = "Admin",
+                LastName="admin",
+                EmailConfirmed = true
+            };
+        var user= await userManager.FindByIdAsync(admin.Id);
+        if(user is null)
+        {
+            await userManager.CreateAsync(admin, "P@ssword123");
+            await userManager.AddToRoleAsync(admin, AppRoles.Admin);
+        }
+    }
+}
+ 
