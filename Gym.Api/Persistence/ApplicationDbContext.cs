e9a1841683236bc41a845b1742c16d07cf4d2477
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-07 22:47:18 +0200

    feat(migration): add migration for ApplicationUser entity

--- Gym.Api/Persistence/ApplicationDbContext.cs
+++ Gym.Api/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,17 @@
 ﻿using Gym.Api.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Reflection.Emit;
 namespace Gym.Api.Persistence;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser> (options)
 {
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+       
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
 
 }
