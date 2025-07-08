0a4264ab3794406ae82120aab001b2b2c527ad74
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 06:37:25 +0200

    fix: implement global exception handler with structured problem details
    - Log exceptions with error level and message
    - Return RFC 7231-compliant ProblemDetails for unhandled exceptions
    - Set HTTP status code to 500 for internal server errors

---
+++ Gym.Api/Errors/GlobalExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Errors;
+
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "something went wrong :{Massage} ", exception.Message);
+
+        var problemDetailes = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Internal Server Error",
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"
+        };
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        await httpContext.Response.WriteAsJsonAsync(problemDetailes, cancellationToken: cancellationToken);
+        return true;
+    }
+}
