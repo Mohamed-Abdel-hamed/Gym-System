237539c95933029cf4c101616754c16d94083bab
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 05:52:02 +0200

    Add SubscriptionPlanError class for error handling
    Introduces a new static class `SubscriptionPlanError` in the `Gym.Api.Errors` namespace. This class provides static properties for various subscription plan-related error messages, including "Not Found", "Already Exists", "Invalid Plan Type", "Invalid Duration", and "Invalid Price", each initialized with a unique error code and message.

---
+++ Gym.Api/Errors/SubscriptionPlanError.cs
@@ -0,0 +1,12 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public static class SubscriptionPlanError
+{
+    public static Error NotFound => new("SubscriptionPlans.NotFound", "Not found any SubscriptionPlans");
+    public static Error AlreadyExists => new("SubscriptionPlans.AlreadyExists", "Subscription plan already exists");
+    public static Error InvalidPlanType => new("SubscriptionPlans.InvalidPlanType", "Invalid subscription plan type");
+    public static Error InvalidDuration => new("SubscriptionPlans.InvalidDuration", "Invalid subscription plan duration");
+    public static Error InvalidPrice => new("SubscriptionPlans.InvalidPrice", "Invalid subscription plan price");
+}
