6f666403a27df978be2e0c8ec7597d15607c10fb
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 02:06:14 +0200

    feat(subscriptionplan): add SubscriptionPlan controller that will handel  requests and add endpoint that return all SubscriptionPlan

---
+++ Gym.Api/Controllers/SubscriptionPlansController.cs
@@ -0,0 +1,16 @@
+﻿using Gym.Api.Services.SubscriptionPlans;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class SubscriptionPlansController(ISubscriptionPlanService planService) : ControllerBase
+{
+    private readonly ISubscriptionPlanService _planService = planService;
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _planService.GetAllAsync());
+    }
+}
