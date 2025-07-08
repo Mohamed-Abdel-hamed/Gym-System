aa818204d83d47b5d8417920b535356a1cffb070
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 01:35:51 +0200

    refactor(result): streamline ResultDetails class

--- Gym.Api/Abstractions/ResultExtensions.cs
+++ Gym.Api/Abstractions/ResultExtensions.cs
@@ -4,24 +4,24 @@ namespace Gym.Api.Abstractions;
 
 public static class ResultExtensions
 {
-    public static ObjectResult ToProblem(this Result result,int statusCode,string title)
+    public static ObjectResult ToProblem(this Result result)
     {
        if(result.IsSuccess)
             throw new InvalidOperationException("cannot convert to Problem");
 
+        var problem = Results.Problem(statusCode:result.Error.statusCode);
 
+        var problemDetails = problem.GetType().GetProperty(nameof(ProblemDetails))!.GetValue(problem) as ProblemDetails;
 
-        ProblemDetails problemDetails = new()
-        {
-            Status = statusCode,
-            Title = title,
-            Extensions = new Dictionary<string, object?>
+        problemDetails!.Extensions = new Dictionary<string, object?>
             {
                 {
-                    "errors",new [] { result.Error}
+                    "errors",new [] { 
+                        result.Error.Code,
+                        result.Error.Description,
+                    }
                 }
-            }
-        };
+            };
 
         return new ObjectResult(problemDetails);
 
