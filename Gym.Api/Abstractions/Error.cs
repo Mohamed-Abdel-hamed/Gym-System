5863b0c4680ec54483bfd56994f980ba5e058b56
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 05:11:39 +0200

    feat(return pattern): add result pattern to centeral error and desgin of returns in all methods

---
+++ Gym.Api/Abstractions/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace Gym.Api.Abstractions;
+
+public record Error (string Code,string Description)
+{
+    public static readonly Error Non = new(string.Empty, string.Empty);
+}
---
+++ Gym.Api/Abstractions/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace Gym.Api.Abstractions;
+
+public class Result
+{
+    public Result(bool isSuccess, Error error)
+    {
+        if ((isSuccess && Error.Non != error) || (!isSuccess && Error.Non == error))
+
+            throw new InvalidOperationException();
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure =>!IsSuccess;
+    public Error Error { get; } = default!;
+    public static Result Success()=>new(true, Error.Non);
+    public static Result Failure(Error error) => new(false,error);
+    public static Result<T> Success<T>(T value) => new(value,true, Error.Non);
+    public static Result<T> Failure<T>(Error error) => new(default, false, error);
+}
+public class Result<T>(T? value, bool isSuccess, Error error) : Result(isSuccess, error)
+{
+
+    private readonly T? _value=value;
+    public T Value=> IsSuccess ? _value! : throw new InvalidOperationException("Failure results can not have value");
+}
