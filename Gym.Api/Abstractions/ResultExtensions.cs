87adb4ea714ee106a6055dc3441e417238cd4076
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 06:18:57 +0200

    Add ResultExtensions for structured error handling
    Introduce a new static class `ResultExtensions` in the
    `Gym.Api.Abstractions` namespace with a method `ToProblem`.
    This method converts a `Result` object into an `ObjectResult`
    with `ProblemDetails`, enhancing error handling in API responses
    by providing a structured representation of errors.

---
+++ Gym.Api/Abstractions/ResultExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Abstractions;
+
+public static class ResultExtensions
+{
+    public static ObjectResult ToProblem(this Result result,int statusCode,string title)
+    {
+       if(result.IsSuccess)
+            throw new InvalidOperationException("cannot convert to Problem");
+
+
+
+        ProblemDetails problemDetails = new()
+        {
+            Status = statusCode,
+            Title = title,
+            Extensions = new Dictionary<string, object?>
+            {
+                {
+                    "errors",new [] { result.Error}
+                }
+            }
+        };
+
+        return new ObjectResult(problemDetails);
+
+    }
+}
