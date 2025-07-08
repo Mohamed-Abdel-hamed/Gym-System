6a6e9b0d0159b1a86fcb7d27cdc2b21bf0c62bb3
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 01:39:20 +0200

    feat(auth): add regitser method

--- Gym.Api/Services/Auth/AuthService.cs
+++ Gym.Api/Services/Auth/AuthService.cs
@@ -1,9 +1,42 @@
-﻿using Gym.Api.Entities;
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Authentications;
+using Gym.Api.Entities;
+using Gym.Api.Errors;
+using Mapster;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace Gym.Api.Services.Auth;
 
-public class AuthService(UserManager<ApplicationUser> userManager) : IAuthService
+public class AuthService(UserManager<ApplicationUser> userManager,ILogger<AuthService> logger) : IAuthService
 {
     private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly ILogger<AuthService> _logger = logger;
+
+    public async Task<Result> RegisterAsync(RegisterRequest request,CancellationToken cancellation=default)
+    {
+        var emailIsExists = await _userManager.Users.AnyAsync(x=>x.Email==request.Email,cancellation);
+
+        if (emailIsExists)
+            return Result.Failure(UserErrors.DuplicatedEmail);
+
+        var phoneIsExists = await _userManager.Users.AnyAsync(x => x.PhoneNumber == request.PhoneNumber, cancellation);
+
+        if (phoneIsExists)
+            return Result.Failure(UserErrors.DuplicatedPhoneNumber);
+        ApplicationUser user=request.Adapt<ApplicationUser>();
+       var result= await _userManager.CreateAsync(user,request.Password);
+        if(result.Succeeded )
+        {
+            var token= await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            token=WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+            _logger.LogInformation("Confirmation Token,{token}",token);
+            return Result.Success();
+        }
+        var error=result.Errors.First();
+        return Result.Failure(new Error(error.Code, error.Description,StatusCodes.Status400BadRequest));
+    }
 }
--- Gym.Api/Services/Auth/IAuthService.cs
+++ Gym.Api/Services/Auth/IAuthService.cs
@@ -1,5 +1,9 @@
-﻿namespace Gym.Api.Services.Auth;
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Authentications;
+
+namespace Gym.Api.Services.Auth;
 
 public interface IAuthService
 {
+    Task<Result> RegisterAsync(RegisterRequest request, CancellationToken cancellation = default);
 }
