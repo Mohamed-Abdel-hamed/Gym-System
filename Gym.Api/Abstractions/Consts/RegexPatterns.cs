3e0382904215839273c129e056b7a1c63bb7e9dd
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 09:06:55 +0200

    feat(validation): add RegexPatterns constants for password & phone rules
    * Introduces `Gym.Api.Abstractions.Consts.RegexPatterns` with:
      - `Password` regex enforcing ≥ 8 chars, uppercase, lowercase, digit and special char.
      - `PhoneNumber` regex (currently same pattern; will refine in future).
    
    These constants centralise validation logic and make it reusable across the API.

---
+++ Gym.Api/Abstractions/Consts/RegexPatterns.cs
@@ -0,0 +1,7 @@
+﻿namespace Gym.Api.Abstractions.Consts;
+
+public static class RegexPatterns
+{
+    public const string Password = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
+    public const string PhoneNumber = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
+}
