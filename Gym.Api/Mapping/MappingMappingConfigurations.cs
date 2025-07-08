a0ad48b0dd4d398aafc656d3c2cc7a9622decd38
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 01:38:45 +0200

    feat(mapping): add mapping by mapster to map dto

---
+++ Gym.Api/Mapping/MappingMappingConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using Gym.Api.Contracts.Authentications;
+using Gym.Api.Entities;
+using Mapster;
+
+namespace Gym.Api.Mapping;
+
+public class MappingMappingConfigurations : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<RegisterRequest,ApplicationUser>()
+            .Map(dest=>dest.UserName,src=>src.Email);
+    }
+}
