00b845be25ba67bc4e75002dc6ffa0fe1cb4b74f
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 02:04:19 +0200

    feat(subscriptionplan): add SubscriptionPlan service that contain logic and add getall method that return all SubscriptionPlans

---
+++ Gym.Api/Services/SubscriptionPlans/ISubscriptionPlanService.cs
@@ -0,0 +1,8 @@
+﻿using Gym.Api.Contracts.SubscriptionPlans;
+
+namespace Gym.Api.Services.SubscriptionPlans;
+
+public interface ISubscriptionPlanService
+{
+    Task<IEnumerable<SubscriptionPlanResponse>> GetAllAsync(CancellationToken cancellation=default);
+}
---
+++ Gym.Api/Services/SubscriptionPlans/SubscriptionPlanService.cs
@@ -0,0 +1,32 @@
+﻿using Gym.Api.Contracts.SubscriptionPlans;
+using Gym.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Gym.Api.Services.SubscriptionPlans;
+
+public class SubscriptionPlanService(ApplicationDbContext context) : ISubscriptionPlanService
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<IEnumerable<SubscriptionPlanResponse>> GetAllAsync(CancellationToken cancellation = default)
+    {
+       var subscriptionPlans = await _context.SubscriptionPlans
+            .AsNoTracking()
+            .Select(x=>new SubscriptionPlanResponse
+            (
+                x.Name,
+                x.UnlimitedDailyEntries,
+                x.MaxClassBookingsPerDay,
+                x.MaxClassBookingsInFuture,
+                x.MaxFreezesPerYear,
+                x.MaxFreezeDays
+            )
+            )
+            .ToListAsync(cancellation);
+        if(subscriptionPlans.Count==0)
+
+            return [];
+        return subscriptionPlans;
+    }
+}
