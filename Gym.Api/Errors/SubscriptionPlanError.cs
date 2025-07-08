88d8f5de7bae9a5664ea2f8e65987ead355a5a91
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 01:36:10 +0200

    feat(error): add StatusCode property to Error class

--- Gym.Api/Errors/SubscriptionPlanError.cs
+++ Gym.Api/Errors/SubscriptionPlanError.cs
@@ -4,9 +4,9 @@ namespace Gym.Api.Errors;
 
 public static class SubscriptionPlanError
 {
-    public static Error NotFound => new("SubscriptionPlans.NotFound", "Not found any SubscriptionPlans");
-    public static Error AlreadyExists => new("SubscriptionPlans.AlreadyExists", "Subscription plan already exists");
-    public static Error InvalidPlanType => new("SubscriptionPlans.InvalidPlanType", "Invalid subscription plan type");
-    public static Error InvalidDuration => new("SubscriptionPlans.InvalidDuration", "Invalid subscription plan duration");
-    public static Error InvalidPrice => new("SubscriptionPlans.InvalidPrice", "Invalid subscription plan price");
+    public static Error NotFound => new("SubscriptionPlans.NotFound", "Not found any SubscriptionPlans",StatusCodes.Status404NotFound);
+    public static Error AlreadyExists => new("SubscriptionPlans.AlreadyExists", "Subscription plan already exists",StatusCodes.Status409Conflict);
+    public static Error InvalidPlanType => new("SubscriptionPlans.InvalidPlanType", "Invalid subscription plan type", StatusCodes.Status400BadRequest);
+    public static Error InvalidDuration => new("SubscriptionPlans.InvalidDuration", "Invalid subscription plan duration", StatusCodes.Status400BadRequest);
+    public static Error InvalidPrice => new("SubscriptionPlans.InvalidPrice", "Invalid subscription plan price", StatusCodes.Status400BadRequest);
 }
