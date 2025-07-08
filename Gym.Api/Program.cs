25e9d0644c81e69e489383e80a3e5237adba7a6f
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-08 08:30:16 +0200

    Add Serilog logging configuration
    Updated `Program.cs` to integrate Serilog for enhanced logging capabilities. Modified `appsettings.json` to replace the existing logging section with a new "Serilog" section, specifying minimum logging levels and output targets.

--- Gym.Api/Program.cs
+++ Gym.Api/Program.cs
@@ -3,12 +3,18 @@ using Gym.Api.Entities;
 using Gym.Api.Persistence;
 using Gym.Api.Seeds;
 using Microsoft.AspNetCore.Identity;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddServices(builder.Configuration);
 builder.Services.AddControllers();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
