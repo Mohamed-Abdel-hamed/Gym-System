475e18110b81c84252a81dace05a87773468e1cf
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 01:38:14 +0200

    refactor(auth): make return dymnic to return any error auto

---
+++ Gym.Api/Contracts/AuthsController.cs
@@ -0,0 +1,20 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Authentications;
+using Gym.Api.Errors;
+using Gym.Api.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Contracts;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthsController(IAuthService authService) : ControllerBase
+{
+    private readonly IAuthService _authService = authService;
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody]RegisterRequest request,CancellationToken cancellation)
+    {
+        var result=await _authService.RegisterAsync(request,cancellation);
+
+            return result.IsSuccess? Ok():result.ToProblem();
+    }
+}
