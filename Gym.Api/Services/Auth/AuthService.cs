560c88b67e27f3be205a1a4acebbc9049826f1da
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 03:14:52 +0200

    feat(auth): add confirm email to make user signin

--- Gym.Api/Services/Auth/AuthService.cs
+++ Gym.Api/Services/Auth/AuthService.cs
@@ -39,4 +39,31 @@ public class AuthService(UserManager<ApplicationUser> userManager,ILogger<AuthSe
         var error=result.Errors.First();
         return Result.Failure(new Error(error.Code, error.Description,StatusCodes.Status400BadRequest));
     }
+    public async Task<Result> ConfirmEmailAsync(ConfirmEmailRequest request)
+    {
+        if(await _userManager.FindByIdAsync(request.UserId) is not { }user)
+            return Result.Failure(UserErrors.InvalidCode);
+
+        if(user.EmailConfirmed)
+            return Result.Failure(UserErrors.DuplicatedConfirmation);
+
+        var token = request.Token;
+
+        try
+        {
+            token=Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(token));
+        }
+        catch (FormatException)
+        {
+            return Result.Failure(UserErrors.InvalidCode);
+        }
+
+        var result=await _userManager.ConfirmEmailAsync(user,token);
+
+        if(result.Succeeded ) 
+           return Result.Success();
+
+        var error = result.Errors.First();
+        return Result.Failure(new Error(error.Code, error.Description, StatusCodes.Status400BadRequest));
+    }
 }
--- Gym.Api/Services/Auth/IAuthService.cs
+++ Gym.Api/Services/Auth/IAuthService.cs
@@ -6,4 +6,5 @@ namespace Gym.Api.Services.Auth;
 public interface IAuthService
 {
     Task<Result> RegisterAsync(RegisterRequest request, CancellationToken cancellation = default);
+    Task<Result> ConfirmEmailAsync(ConfirmEmailRequest request);
 }
