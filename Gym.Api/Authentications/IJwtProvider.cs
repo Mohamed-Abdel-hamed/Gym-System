0713613a608f78eae3ae5201d1fe25c65787f72a
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-10 01:41:36 +0200

    add(auth): add method to generate token jwt

---
+++ Gym.Api/Authentications/IJwtProvider.cs
@@ -0,0 +1,8 @@
+﻿using Gym.Api.Entities;
+
+namespace Gym.Api.Authentications;
+
+public interface IJwtProvider
+{
+   public (string token,int ExpiresIn) GenerateToken(ApplicationUser user);
+}
---
+++ Gym.Api/Authentications/JwtProvider.cs
@@ -0,0 +1,36 @@
+﻿using Gym.Api.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Gym.Api.Authentications;
+
+public class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
+{
+    private readonly JwtOptions _options = options.Value;
+
+    public (string token, int ExpiresIn) GenerateToken(ApplicationUser user)
+    {
+        Claim[] claims = [
+            new(JwtRegisteredClaimNames.Sub,user.Id),
+            new(JwtRegisteredClaimNames.Email,user.Email!),
+            new(JwtRegisteredClaimNames.GivenName,user.FirstName),
+            new(JwtRegisteredClaimNames.FamilyName,user.LastName),
+            new(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            ];
+
+        var symmtricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Key));
+        var signingCredentials = new SigningCredentials(symmtricSecurityKey,SecurityAlgorithms.HmacSha256);
+        var expiresIn = 30;
+        var token = new JwtSecurityToken(
+            issuer: _options.Issuer,
+            audience: _options.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(expiresIn),
+            signingCredentials: signingCredentials
+            );
+        return (token:new JwtSecurityTokenHandler().WriteToken(token),expiresIn*60);
+    }
+}
