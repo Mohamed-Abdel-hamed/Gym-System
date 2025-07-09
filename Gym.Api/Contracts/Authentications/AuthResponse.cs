1e430c5ab11369b34f0f6b9772a7ea318085e1ff
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-09 14:41:38 +0200

    feat(auth): add response to login

---
+++ Gym.Api/Contracts/Authentications/AuthResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym.Api.Contracts.Authentications;
+
+public record AuthResponse
+(
+    string Id,
+    string?Email,
+    string FirstName,
+    string LastName,
+    string Token,
+    int ExpiresIn
+    );
+
