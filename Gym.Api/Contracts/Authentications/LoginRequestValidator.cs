567570afda69402efd426482da727d644a15118a
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-10 01:42:06 +0200

    feat(auth): add login request to user can log in

---
+++ Gym.Api/Contracts/Authentications/LoginRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Gym.Api.Contracts.Authentications;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
