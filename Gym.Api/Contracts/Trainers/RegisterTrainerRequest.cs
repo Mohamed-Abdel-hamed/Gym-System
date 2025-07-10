fa9f79e2b2c7cc2a1b3b38d8fabe49074a6072fd
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-10 22:22:53 +0200

    feat(auth):add trainer register  container user info and other property

---
+++ Gym.Api/Contracts/Trainers/RegisterTrainerRequest.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Contracts.Authentications;
+
+namespace Gym.Api.Contracts.Trainers;
+
+public record RegisterTrainerRequest
+    (
+    RegisterRequest Info,
+    DateOnly HireDate
+    );
