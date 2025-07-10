d72066492df52d038da56680b6ae4ae494bcbe12
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-10 04:40:13 +0200

    fix(auth):fix regex pattren to phonenumber

--- Gym.Api/Abstractions/Consts/RegexPatterns.cs
+++ Gym.Api/Abstractions/Consts/RegexPatterns.cs
@@ -3,5 +3,5 @@
 public static class RegexPatterns
 {
     public const string Password = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
-    public const string PhoneNumber = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
+    public const string PhoneNumber = "^01[0-9]{9}$";
 }
