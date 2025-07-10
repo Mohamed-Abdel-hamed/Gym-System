94230c8a5a2d23ee0c025d36a3c1052c1decf3bd
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-10 22:45:03 +0200

    feat(auth): add staff request register

---
+++ Gym.Api/Contracts/Staffs/RegisterStaffRequest.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Contracts.Authentications;
+
+namespace Gym.Api.Contracts.Staffs;
+
+public record RegisterStaffRequest
+    (
+    RegisterRequest Info,
+   DateOnly HireDate
+    );
