f4fa382777e367a0ed7a3649aaf77e3ee6117f51
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-10 05:35:13 +0200

    feat(member):add member entity who can booking and subscribe

---
+++ Gym.Api/Entities/Member.cs
@@ -0,0 +1,9 @@
+﻿namespace Gym.Api.Entities;
+
+public class Member
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }=string.Empty;
+    public ApplicationUser User { get; set; } = default!;
+}
+ 
