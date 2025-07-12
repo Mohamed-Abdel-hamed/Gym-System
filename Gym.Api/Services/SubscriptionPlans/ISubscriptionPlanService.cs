db1d726f505aedf566d19f583b8787e67b6ad1c5
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-12 18:32:45 +0200

    feat(subscriptionplane):add update srevice and delete

--- Gym.Api/Services/SubscriptionPlans/ISubscriptionPlanService.cs
+++ Gym.Api/Services/SubscriptionPlans/ISubscriptionPlanService.cs
@@ -7,5 +7,7 @@ public interface ISubscriptionPlanService
 {
     Task<Result<IEnumerable<SubscriptionPlanResponse>>> GetAllAsync(CancellationToken cancellation=default);
     Task<Result> AddAsync(SubscriptionPlanRequest request,CancellationToken cancellation =default);
+    Task<Result> UpdateAsync(int id, SubscriptionPlanRequest request, CancellationToken cancellation = default);
+    Task<Result> DeleteAsync(int id, CancellationToken cancellation = default);
 
 }
--- Gym.Api/Services/SubscriptionPlans/SubscriptionPlanService.cs
+++ Gym.Api/Services/SubscriptionPlans/SubscriptionPlanService.cs
@@ -38,4 +38,40 @@ public class SubscriptionPlanService(ApplicationDbContext context) : ISubscripti
 
         return Result.Success();
     }
+    public async Task<Result> UpdateAsync(int id,SubscriptionPlanRequest request, CancellationToken cancellation = default)
+    {
+        var isExistsSubscriptionPlan = await _context.SubscriptionPlans.AnyAsync(x => x.Name == request.Name&&x.Id!=id, cancellation);
+
+        if (isExistsSubscriptionPlan)
+
+            return Result.Failure(SubscriptionPlanError.AlreadyExists);
+
+        var subscriptionPlan = await _context.SubscriptionPlans
+       .FirstOrDefaultAsync(x => x.Id == id, cancellation);
+
+        if (subscriptionPlan is null)
+            return Result.Failure(SubscriptionPlanError.NotFound);
+
+        subscriptionPlan=request.Adapt(subscriptionPlan);
+
+
+        await _context.SaveChangesAsync(cancellation);
+
+        return Result.Success();
+    }
+    public async Task<Result> DeleteAsync(int id, CancellationToken cancellation = default)
+    {
+        var subscriptionPlan = await _context.SubscriptionPlans
+       .FirstOrDefaultAsync(x => x.Id == id, cancellation);
+
+        if (subscriptionPlan is null)
+            return Result.Failure(SubscriptionPlanError.NotFound);
+
+       _context.SubscriptionPlans.Remove(subscriptionPlan);
+
+
+        await _context.SaveChangesAsync(cancellation);
+
+        return Result.Success();
+    }
 }
