6065857322a263db63abe02b57e222f2a6e359a1
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-13 06:25:12 +0200

    feat(membership):add new error

---
+++ Gym.Api/Errors/MembershipErrors.cs
@@ -0,0 +1,12 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public class MembershipErrors
+{
+    public static Error NotFound => new("Memberships.NotFound", "Not found any Memberships", StatusCodes.Status404NotFound);
+    public static Error NotCompeleteSubscription => new("Memberships.NotCompelete Subscription", "Not Compelete Subscription", StatusCodes.Status404NotFound);
+    public static Error AlreadyExists => new("Memberships.AlreadyExists", "Membership already exists", StatusCodes.Status409Conflict);
+    public static Error InvalidPlanType => new("Memberships.InvalidPlanType", "Invalid Membership type", StatusCodes.Status400BadRequest);
+    public static Error InvalidAutoRenewal => new("Memberships.InvalidAutoRenewal", "This plan does not support auto-renewal", StatusCodes.Status400BadRequest);
+}
