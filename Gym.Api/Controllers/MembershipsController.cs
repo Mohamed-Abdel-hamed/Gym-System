b232e595ee9f52f0ea405921f4e7c0573f01825f
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-13 06:29:00 +0200

    feat(membership):add subscribe endpoint to can member subscribe

---
+++ Gym.Api/Controllers/MembershipsController.cs
@@ -0,0 +1,36 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Abstractions.Consts;
+using Gym.Api.Contracts.Memberships;
+using Gym.Api.Services.Memberships;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Gym.Api.Controllers;
+[Route("api/SubscriptionPlane/{planeId}/[controller]")]
+[ApiController]
+//[Authorize(Roles =AppRoles.Member)]
+public class MembershipsController(IMembershipService _membershipService) : ControllerBase
+{
+    private readonly IMembershipService _membershipService = _membershipService;
+    [HttpPost("subscribe")]
+    public async Task<IActionResult> Subscribe([FromRoute]int planeId,[FromBody] MembershipRequest request, CancellationToken cancellation = default)
+    {
+        var userId= User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        var result=await _membershipService.SubscribeAsync(planeId,request.AutoRenew,userId,cancellation);
+        return result.IsSuccess? Ok(new { url = result.Value }): result.ToProblem();
+    }
+    [HttpGet("success")]
+    public async Task<IActionResult> Success(int id)
+    {
+        var result = await _membershipService.SuccessAsync(id);
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+    [HttpGet]
+    public  IActionResult Error()
+    {
+        return Ok("Error Proccess");
+    }
+
+}
