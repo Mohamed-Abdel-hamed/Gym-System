9dd938845ff519d1f40a2f229081dd152c03fa23
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-13 10:51:52 +0200

    feat(membershipfreeze)add controle of freeze to membership

---
+++ Gym.Api/Services/MembershipFreezes/IMembershipFreezeService.cs
@@ -0,0 +1,8 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Services.MembershipFreezes;
+
+public interface IMembershipFreezeService
+{
+    Task<Result> AddAsync(int memberId,int membershipId, CancellationToken cancellation = default);
+}
---
+++ Gym.Api/Services/MembershipFreezes/MembershipFreezeService.cs
@@ -0,0 +1,15 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym.Api.Services.MembershipFreezes;
+
+public class MembershipFreezeService(ApplicationDbContext context) : IMembershipFreezeService
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<Result> AddAsync(int memberId, int membershipId,CancellationToken cancellation=default)
+    {
+         var isExistsMembership=await _context.Memberships.SingleOrDefaultAsync()
+    }
+}
