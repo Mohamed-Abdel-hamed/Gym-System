38761f3f82d63e0c8da6fb451f7934aabfd901a3
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-14 22:43:44 +0200

    feat: make title in class 100 chars

---
+++ Gym.Api/Persistence/EntitiesConfiguration/ClassConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Gym.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gym.Api.Persistence.EntitiesConfiguration;
+
+public class ClassConfiguration : IEntityTypeConfiguration<Class>
+{
+    public void Configure(EntityTypeBuilder<Class> builder)
+    {
+        builder.Property(x => x.Title).HasMaxLength(100);
+
+    }
+}
