ca0b1b8ce661aa8254390dc776812ba610908f96
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-15 00:25:33 +0200

    feat: add types of errors to clear reason failure

---
+++ Gym.Api/Errors/ClassErrors.cs
@@ -0,0 +1,10 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public class ClassErrors
+{
+    public static Error NotFound => new("classes.NotFound", "Not found any clesses", StatusCodes.Status404NotFound);
+    public static Error AlreadyExists => new("class.AlreadyExists", "class plan already exists", StatusCodes.Status409Conflict);
+
+}
---
+++ Gym.Api/Errors/TrainerErrors.cs
@@ -0,0 +1,8 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public class TrainerErrors
+{
+    public static Error NotFound => new("trainer.NotFound", "Not found any trainer", StatusCodes.Status404NotFound);
+}
