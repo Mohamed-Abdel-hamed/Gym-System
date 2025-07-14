17852f203f23117bde020ceac356a454cde1020c
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-15 00:29:30 +0200

    feat: add endpiont to add class

---
+++ Gym.Api/Controllers/ClassesController.cs
@@ -0,0 +1,25 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Abstractions.Consts;
+using Gym.Api.Contracts.Classes;
+using Gym.Api.Services.Classes;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles =AppRoles.Staff)]
+public class ClassesController(IClassService classService) : ControllerBase
+{
+    private readonly IClassService _classService = classService;
+
+    [HttpPost("trainer/{trainerId}")]
+    public async Task<IActionResult> Add([FromRoute]int trainerId,[FromBody] ClassRequest request,CancellationToken cancellation=default)
+    {
+        var result = await _classService.AddAsync(trainerId, request, cancellation);
+
+        return result.IsSuccess ? Ok() : result.ToProblem();
+
+    }
+}
