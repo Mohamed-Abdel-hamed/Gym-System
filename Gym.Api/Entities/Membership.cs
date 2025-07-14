c95ada6930f2d6457e44bd4662f6bb2b04fc37ce
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-14 03:16:35 +0200

    refct: membership entity

--- Gym.Api/Entities/Membership.cs
+++ Gym.Api/Entities/Membership.cs
@@ -19,7 +19,7 @@ public class Membership
 
     // Auto-renewal options (per user instance)
     public bool AutoRenew { get; set; } = false;
-    public bool AutoRenewPaid { get; set; } = false;
+    public bool AutoRenewPaid { get; set; } = false; 
     public ICollection<Payment> Payments { get; set; } = [];
     public ICollection<MembershipFreeze> Freezes { get; set; } = [];
 }
