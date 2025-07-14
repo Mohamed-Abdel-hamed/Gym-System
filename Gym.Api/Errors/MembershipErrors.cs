cefcb95a0b5958a85fdf82de43bf4d478a535fd4
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-14 03:15:16 +0200

    feat(api): add endpoint to freeze membership

--- Gym.Api/Errors/MembershipErrors.cs
+++ Gym.Api/Errors/MembershipErrors.cs
@@ -4,9 +4,11 @@ namespace Gym.Api.Errors;
 
 public class MembershipErrors
 {
-    public static Error NotFound => new("Memberships.NotFound", "Not found any Memberships", StatusCodes.Status404NotFound);
-    public static Error NotCompeleteSubscription => new("Memberships.NotCompelete Subscription", "Not Compelete Subscription", StatusCodes.Status404NotFound);
+    public static Error NotFound => new("Memberships.NotFound", "Not found any memberships", StatusCodes.Status404NotFound);
+    public static Error NotActive => new("Memberships.NotActive", "Not active memberships", StatusCodes.Status400BadRequest);
+    public static Error LimitDuration => new("Memberships.LimitDuration", "Duration of  memberships less than year", StatusCodes.Status400BadRequest);
+    public static Error NotCompeleteSubscription => new("Memberships.NotCompelete Subscription", "Not compelete subscription", StatusCodes.Status400BadRequest);
     public static Error AlreadyExists => new("Memberships.AlreadyExists", "Membership already exists", StatusCodes.Status409Conflict);
-    public static Error InvalidPlanType => new("Memberships.InvalidPlanType", "Invalid Membership type", StatusCodes.Status400BadRequest);
+    public static Error InvalidPlanType => new("Memberships.InvalidPlanType", "Invalid membership type", StatusCodes.Status400BadRequest);
     public static Error InvalidAutoRenewal => new("Memberships.InvalidAutoRenewal", "This plan does not support auto-renewal", StatusCodes.Status400BadRequest);
 }
---
+++ Gym.Api/Errors/MembershipFreezeErrors.cs
@@ -0,0 +1,10 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public class MembershipFreezeErrors
+{
+    public static Error NotAllowedFreeze => new("Memberships. NotAllowedFreeze", "Remaining subscription period memberships less than max freeze days of plan", StatusCodes.Status400BadRequest);
+    public static Error NotExistsFreeze => new("Memberships. NotExistsFreeze", "Not exists freeze to this plane", StatusCodes.Status404NotFound);
+    public static Error NotAllowedFreezeCount => new("Memberships. NotAllowedFreezeCount", " Not allowed  freeze memberships  count of freeze greater than max freeze per year", StatusCodes.Status400BadRequest);
+}
