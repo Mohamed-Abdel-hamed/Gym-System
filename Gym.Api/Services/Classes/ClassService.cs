6613135da6ef0ba4df01f12d53c804376b119645
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-15 00:28:01 +0200

    feat: add method to add class to trainer  by staff

---
+++ Gym.Api/Services/Classes/ClassService.cs
@@ -0,0 +1,34 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Classes;
+using Gym.Api.Entities;
+using Gym.Api.Errors;
+using Gym.Api.Persistence;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym.Api.Services.Classes;
+
+public class ClassService(ApplicationDbContext context) : IClassService
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<Result> AddAsync(int trainerId,ClassRequest request, CancellationToken cancellation = default)
+    {
+        var isExistsTrainer = await _context.Trainers.AnyAsync(x => x.Id == trainerId, cancellation);
+        if (!isExistsTrainer)
+            return Result.Failure(TrainerErrors.NotFound);
+
+        var isExistsClass=await _context.Classes.AnyAsync(x=>x.Title == request.Title&&x.TrainerId == trainerId,cancellation);
+
+        if (isExistsClass)
+            return Result.Failure(ClassErrors.AlreadyExists);
+
+        Class newClass = request.Adapt<Class>();
+        newClass.TrainerId = trainerId;
+        await _context.Classes.AddAsync(newClass,cancellation);
+        await _context.SaveChangesAsync(cancellation);
+
+        return Result.Success();
+
+    }
+}
---
+++ Gym.Api/Services/Classes/IClassService.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Classes;
+
+namespace Gym.Api.Services.Classes;
+
+public interface IClassService
+{
+    Task<Result> AddAsync(int trainerId, ClassRequest request,CancellationToken cancellation=default);
+}
