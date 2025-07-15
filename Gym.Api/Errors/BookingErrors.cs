a059cdfc594fce2546e9e6bdbbcd28450dab8f74
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-15 06:08:06 +0200

    fix: move error details

---
+++ Gym.Api/Errors/BookingErrors.cs
@@ -0,0 +1,8 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public class BookingErrors
+{
+    public static Error AlreadyExists => new("Booking.AlreadyExists", "Booking already exists", StatusCodes.Status409Conflict);
+}
--- Gym.Api/Errors/ClassErrors.cs
+++ Gym.Api/Errors/ClassErrors.cs
@@ -6,5 +6,7 @@ public class ClassErrors
 {
     public static Error NotFound => new("classes.NotFound", "Not found any clesses", StatusCodes.Status404NotFound);
     public static Error AlreadyExists => new("class.AlreadyExists", "class plan already exists", StatusCodes.Status409Conflict);
+    public static Error CompleteClass => new("Class.CompleteClass", "can not booking because class is comleted", StatusCodes.Status400BadRequest);
+    public static Error AlreadyStarted => new("Class.AlreadyStarted", "can not booking because class already started", StatusCodes.Status400BadRequest);
 
 }
