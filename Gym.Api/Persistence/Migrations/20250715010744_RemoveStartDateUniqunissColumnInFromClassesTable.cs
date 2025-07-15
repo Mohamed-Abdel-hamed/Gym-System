105a93358857b534b16560d06b4f7c2c0e5d6d01
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-15 03:09:04 +0200

    fix: remove uniquniss from startdate in class

---
+++ Gym.Api/Persistence/Migrations/20250715010744_RemoveStartDateUniqunissColumnInFromClassesTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym.Api.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveStartDateUniqunissColumnInFromClassesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Classes_TrainerId_Title_StartDate",
+                table: "Classes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_TrainerId_Title",
+                table: "Classes",
+                columns: new[] { "TrainerId", "Title" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Classes_TrainerId_Title",
+                table: "Classes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_TrainerId_Title_StartDate",
+                table: "Classes",
+                columns: new[] { "TrainerId", "Title", "StartDate" },
+                unique: true);
+        }
+    }
+}
