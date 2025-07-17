16820138a6af09b3c1f7ca81edccb820be8fdd27
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-17 07:11:58 +0200

    feat: add endpoint to get member profile

---
+++ Gym.Api/Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Abstractions.Consts;
+using Gym.Api.Services.Members;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles =AppRoles.Member)]
+public class MembersController(IMemberSerive memberSerive) : ControllerBase
+{
+    private readonly IMemberSerive _memberSerive = memberSerive;
+
+    [HttpGet("")]
+    public async Task<IActionResult> Get(CancellationToken cancellation=default)
+    {
+        var userId=User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        var result = await _memberSerive.GetAsync(userId,cancellation);
+
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+}
