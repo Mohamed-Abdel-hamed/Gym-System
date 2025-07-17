0d2fb9c009694a0b78b868782fd8524c26f00e04
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-17 09:52:57 +0200

    feat: add endpoint to retun trainer info

---
+++ Gym.Api/Controllers/TrainersController.cs
@@ -0,0 +1,24 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Abstractions.Consts;
+using Gym.Api.Services.Trainers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles =AppRoles.Trainer)]
+public class TrainersController(ITrainerService trainerService) : ControllerBase
+{
+    private readonly ITrainerService _trainerService = trainerService;
+    [HttpGet("")]
+    public async Task<IActionResult> Get(CancellationToken cancellation = default)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        var result = await _trainerService.GetAsync(userId,cancellation);
+
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+}
