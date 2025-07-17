e2b06c61665dded683ed958861473a0db9355cf3
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-17 07:09:21 +0200

    feat:add member error to describe type of errors

---
+++ Gym.Api/Errors/MemberErros.cs
@@ -0,0 +1,8 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public class MemberErros
+{
+    public static Error NotFound => new("member.NotFound", "Not found any member", StatusCodes.Status404NotFound);
+}
