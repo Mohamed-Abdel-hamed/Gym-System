0ca9d69cdd0eb5efa6e674e05321e72dccc2976a
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-17 07:10:11 +0200

    feat: add get method to return  member profile

---
+++ Gym.Api/Services/Members/IMemberSerive.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Members;
+
+namespace Gym.Api.Services.Members;
+
+public interface IMemberSerive
+{
+    Task<Result<MemberResponse>> GetAsync(string userId,CancellationToken cancellation=default);
+}
---
+++ Gym.Api/Services/Members/MemberSerive.cs
@@ -0,0 +1,31 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Members;
+using Gym.Api.Errors;
+using Gym.Api.Persistence;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym.Api.Services.Members;
+
+public class MemberSerive(ApplicationDbContext context) : IMemberSerive
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<Result<MemberResponse>> GetAsync(string userId, CancellationToken cancellation = default)
+    {
+       var member=await _context.Members
+            .AsNoTracking()
+            .Where(m=>m.UserId == userId)
+            .Include(m=>m.User)
+            .Include(m=>m.Bookings)
+            .ThenInclude(m=>m.Class)
+            .ThenInclude(m=>m.Trainer)
+            .ProjectToType<MemberResponse>()
+            .SingleOrDefaultAsync(cancellation);
+
+        if (member is null)
+            return Result.Failure<MemberResponse>(MemberErros.NotFound);
+
+        return Result.Success(member);
+    }
+}
