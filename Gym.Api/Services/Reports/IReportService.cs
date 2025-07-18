7a50d5d59e5c3fba4a75c5f1562824b12c1eaf02
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-18 11:10:45 +0200

    feat: add report service to get summry and statics data

---
+++ Gym.Api/Services/Reports/IReportService.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Dashboards;
+
+namespace Gym.Api.Services.Reports;
+
+public interface IReportService
+{
+    Task<Result<DashboardSummary>> GetDashboardSummary();
+}
---
+++ Gym.Api/Services/Reports/ReportService.cs
@@ -0,0 +1,21 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Dashboards;
+using Gym.Api.Persistence;
+using Gym.Api.Services.Memberships;
+
+namespace Gym.Api.Services.Reports;
+
+public class ReportService(IMembershipService _membershipService) : IReportService
+{
+    private readonly IMembershipService _membershipService = _membershipService;
+
+    public async Task<Result<DashboardSummary>> GetDashboardSummary()
+    {
+        var numberOfActiveMemberships=await _membershipService.GetNumberOfActiveMemberships();
+        var numberOfExpiredMemberships = await _membershipService.GetNumberOfExpiredMemberships();
+
+        DashboardSummary dashboardSummary=new(numberOfActiveMemberships, numberOfExpiredMemberships);
+
+        return Result.Success(dashboardSummary);
+    }
+}
