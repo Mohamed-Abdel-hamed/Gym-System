cbda71ff959a52c1e3531ce97f9cd6144dc229b4
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-18 04:51:38 +0200

    feat:add method ins ervice user to update user profile

---
+++ Gym.Api/Services/Users/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Users;
+
+namespace Gym.Api.Services.Users;
+
+public interface IUserService
+{
+    Task<Result> UpdateAsync(string userId, UpdateUserRequest request);
+}
---
+++ Gym.Api/Services/Users/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Users;
+using Gym.Api.Entities;
+using Gym.Api.Errors;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
+
+namespace Gym.Api.Services.Users;
+
+public class UserService(UserManager<ApplicationUser> userManager): IUserService
+{
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+
+    public async Task<Result> UpdateAsync(string userId, UpdateUserRequest request)
+    {
+        if (await _userManager.FindByIdAsync(userId) is not { } user)
+            return Result.Failure(UserErrors.UserNotFound);
+
+        user = request.Adapt(user);
+
+        var result = await _userManager.UpdateAsync(user);
+
+        if (result.Succeeded)
+            return Result.Success();
+
+
+        var error = result.Errors.First();
+
+        return Result.Failure(new Error(error.Code, error.Description, StatusCodes.Status400BadRequest));
+    }
+}
