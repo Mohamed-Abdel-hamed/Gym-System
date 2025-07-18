c6347f902bc0eeeff02c694c22468ee19b6e555b
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-18 11:11:52 +0200

    feat: add endpoint that return dashboard summary

---
+++ Gym.Api/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Services.Memberships;
+using Gym.Api.Services.Reports;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class DashboardController(IMembershipService membershipService,
+    IReportService reportService) : ControllerBase
+{
+    private readonly IMembershipService _membershipService = membershipService;
+    private readonly IReportService _reportService = reportService;
+
+    [HttpGet("memberships-per-day")]
+    public async Task<IActionResult>MembershipsPerDay(DateTime? startDate, DateTime? endDate)
+    {
+        var result=await _membershipService.MembershipsPerDay(startDate, endDate);
+        return result.IsSuccess? Ok(result.Value) : result.ToProblem();
+    }
+    [HttpGet("dashboard-overview")]
+    public async Task<IActionResult> DashboardOverview()
+    {
+        var result = await _reportService.GetDashboardSummary();
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+}
