c8fd59c2d48a8c0a863c680b869592222c23417a
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-19 21:16:16 +0200

    feat: add endpoint to report memeberships response to show data and report it to excel file

---
+++ Gym.Api/Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Services.Reports;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ReportsController(IReportService reportService) : ControllerBase
+{
+    private readonly IReportService _reportService = reportService;
+
+    [HttpGet("")]
+    public async Task<IActionResult> DownloadMemberships()
+    {
+        var woorkBook = await _reportService.ExportMembershipsToExcel();
+
+        await using MemoryStream stream = new ();
+        woorkBook.SaveAs (stream);
+
+        return File(stream.ToArray(),"application/octet-stream","memberships.xlsx");
+    }
+}
