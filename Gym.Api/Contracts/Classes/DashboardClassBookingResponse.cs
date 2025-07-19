5f4989d5e017782afe07785130afc3c403a06b48
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-19 02:40:03 +0200

    feat: add class response to shape data from class to dashboard

---
+++ Gym.Api/Contracts/Classes/DashboardClassBookingResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Gym.Api.Contracts.Classes;
+
+public record DashboardClassBookingResponse
+    (
+     int Id,
+    string Title,
+    DateTime StartDate,
+    int Capacity,
+    double BookingsAverage
+    );
