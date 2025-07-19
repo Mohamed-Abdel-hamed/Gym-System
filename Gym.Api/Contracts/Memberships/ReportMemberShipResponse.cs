9bf77cd1b6bf878ddd30523c56bd7635faeb12cb
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-19 21:14:15 +0200

    feat: add contract to report memeberships response to show data and report it to excel file

---
+++ Gym.Api/Contracts/Memberships/ReportMemberShipResponse.cs
@@ -0,0 +1,14 @@
+﻿using Gym.Api.Abstractions.Consts;
+
+namespace Gym.Api.Contracts.Memberships;
+
+public record ReportMemberShipResponse
+    (
+     string StartDate,
+     string EndDate,
+     bool AutoRenewPaid,
+     string Status,
+     string Member,
+     string Plane,
+     int NumberOfFreezes
+    );
