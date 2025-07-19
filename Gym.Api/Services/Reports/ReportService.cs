f8b378d375f1ba85bf3db42a7b8526eef568e323
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-19 03:09:35 +0200

    feat: add method to get classes with highestn number of bookings in report service

--- Gym.Api/Services/Reports/ReportService.cs
+++ Gym.Api/Services/Reports/ReportService.cs
@@ -1,21 +1,26 @@
 ﻿using Gym.Api.Abstractions;
 using Gym.Api.Contracts.Dashboards;
 using Gym.Api.Persistence;
+using Gym.Api.Services.Bookings;
 using Gym.Api.Services.Memberships;
 
 namespace Gym.Api.Services.Reports;
 
-public class ReportService(IMembershipService _membershipService) : IReportService
+public class ReportService(IMembershipService _membershipService,IBookingService bookingService) : IReportService
 {
     private readonly IMembershipService _membershipService = _membershipService;
+    private readonly IBookingService _bookingService = bookingService;
 
     public async Task<Result<DashboardSummary>> GetDashboardSummary()
     {
         var numberOfActiveMemberships=await _membershipService.GetNumberOfActiveMemberships();
+
         var numberOfExpiredMemberships = await _membershipService.GetNumberOfExpiredMemberships();
 
-        DashboardSummary dashboardSummary=new(numberOfActiveMemberships, numberOfExpiredMemberships);
+        var classes=await _bookingService.GetClassesWithHighestNumberOfBookings();
 
-        return Result.Success(dashboardSummary);
+        DashboardSummary dashboardSummary=new(numberOfActiveMemberships, numberOfExpiredMemberships,classes);
+
+        return Result.Success(dashboardSummary); 
     }
 }
