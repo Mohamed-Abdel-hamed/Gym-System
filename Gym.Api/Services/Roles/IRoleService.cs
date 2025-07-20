a1e524cdc43bbda79e47dd0ee9d69e2c7be80e65
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-20 06:06:05 +0200

    feat: add role servive and contain method to get roles

---
+++ Gym.Api/Services/Roles/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Roles;
+
+namespace Gym.Api.Services.Roles;
+
+public interface IRoleService
+{
+    Task<Result<IEnumerable<RoleResponse>>> GetAllAsync();
+}
---
+++ Gym.Api/Services/Roles/RoleService.cs
@@ -0,0 +1,26 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Contracts.Roles;
+using Gym.Api.Errors;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym.Api.Services.Roles;
+
+public class RoleService(RoleManager<IdentityRole> roleManager) : IRoleService
+{
+    private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+    public async Task<Result<IEnumerable<RoleResponse>>> GetAllAsync()
+    {
+      var roles=  await _roleManager.Roles
+            .AsNoTracking()
+            .ProjectToType<RoleResponse>()
+            .ToListAsync();
+        if (roles.Count == 0)
+            return Result.Failure<IEnumerable<RoleResponse>>(RoleErrors.NotFound);
+
+        return Result.Success<IEnumerable<RoleResponse>>(roles);
+
+    }
+
+}
