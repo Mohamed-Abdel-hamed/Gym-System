3d67781e5ff66d6d81e8a5a38e500535e4d6c9a6
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-20 06:05:24 +0200

    feat: add roles error to describe errors

---
+++ Gym.Api/Errors/RoleErrors.cs
@@ -0,0 +1,9 @@
+﻿using Gym.Api.Abstractions;
+
+namespace Gym.Api.Errors;
+
+public static class RoleErrors
+{
+    public static readonly Error NotFound =
+    new("Role.RoleNotFound", "Role is not found", StatusCodes.Status404NotFound);
+}
