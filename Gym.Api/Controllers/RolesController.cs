58c3c3ada0df75a42ad757ba61ab672d82233ba0
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-20 06:06:48 +0200

    feat: add endpoint to get all roles

---
+++ Gym.Api/Controllers/RolesController.cs
@@ -0,0 +1,21 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Abstractions.Consts;
+using Gym.Api.Services.Roles;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles =AppRoles.Admin)]  
+public class RolesController(IRoleService roleService) : ControllerBase
+{
+    private readonly IRoleService _roleService = roleService;
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
+    {
+        var result=await _roleService.GetAllAsync();
+        
+        return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+    }
+}
