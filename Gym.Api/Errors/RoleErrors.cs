ea769ac365604cd5e9c3af78a55575d3986f90c0
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-21 02:54:08 +0200

    feat: add type of role error to describe duplicate  role

--- Gym.Api/Errors/RoleErrors.cs
+++ Gym.Api/Errors/RoleErrors.cs
@@ -6,4 +6,7 @@ public static class RoleErrors
 {
     public static readonly Error NotFound =
     new("Role.RoleNotFound", "Role is not found", StatusCodes.Status404NotFound);
+
+    public static readonly Error DuplicatedRole =
+        new("Role.DuplicatedRole", "Another role with the same name is already exists", StatusCodes.Status409Conflict);
 }
