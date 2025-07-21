471f163e095bb4c2af7102332706dfb9280c228e
mohamed <mohamed.saleh1700738@science.menofia.edu.eg>
2025-07-21 05:40:32 +0200

    feat: add endpoint to unlock user

---
+++ Gym.Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Gym.Api.Abstractions;
+using Gym.Api.Abstractions.Consts;
+using Gym.Api.Services.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles =AppRoles.Admin)]
+public class UsersController(IUserService userService) : ControllerBase
+{
+    private readonly IUserService _userService = userService;
+
+    [HttpPut("{userId}/unlock")]
+    public async Task<IActionResult> Unlock(string userId)
+    {
+        var result=await _userService.UnloackAsync(userId);
+
+       return result.IsSuccess? Ok(): result.ToProblem();
+    }
+}
